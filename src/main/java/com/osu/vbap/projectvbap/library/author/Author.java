b36a4ac06c199d4796a24996137c93161dee7463
Yeetulus <l6.mikulec@seznam.cz>
2024-03-09 16:23:41 +0100

    edit

--- src/main/java/com/osu/vbap/projectvbap/library/author/Author.java
+++ src/main/java/com/osu/vbap/projectvbap/library/author/Author.java
@@ -27,8 +27,8 @@ public class Author {
     @ManyToMany
     @JoinTable(
             name = "book_author",
-            joinColumns = @JoinColumn(name = "book_id"),
-            inverseJoinColumns = @JoinColumn(name = "author_id")
+            joinColumns = @JoinColumn(name = "author_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")
     )
     @JsonIgnoreProperties("authors")
     private Set<Book> books;
